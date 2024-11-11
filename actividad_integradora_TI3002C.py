1e0f9468a8b488d1ae02db67aa1cbf47802f7e8d
Fdiaz17 <70969449+Fdiaz17@users.noreply.github.com>
2024-11-10 22:28:21 -0600

    Update actividad_integradora_TI3002C.py

--- actividad_integradora_TI3002C.py
+++ actividad_integradora_TI3002C.py
@@ -1,72 +1,66 @@
-import streamlit as st
 import pandas as pd
+import plotly.express as px
+import streamlit as st
 import numpy as np
-import plotly as px
-import plotly.figure_factory as ff
-from bokeh.plotting import figure
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import seaborn as sns
 import matplotlib.pyplot as plt
+st.set_page_config(layout="wide")
+st.title("Crimes in San Francisco")
+st.markdown("Know the crimes in each area.")
+df=pd.read_csv("Policev1.csv")
+st.sidebar.header("Setting:") #sidebar lo que nos va a hacer es crear en la parte izquierda un cuadro para agregar los filtros que queremos tener
+day_group = df.groupby(['Incident Day of Week'])['Incident Number'].count()
+st.markdown("## Most insecure area.")
+year=df['Incident Year'].unique()
+year_selected = st.sidebar.selectbox('Year', year)
+dist=df['Police District'].unique()
+dist_selected=st.sidebar.selectbox('Area', dist)
 
-st.title('Police Incident Reports from 2018 to 2020 in San Francisco')
+mapa=pd.DataFrame()
 
-df = pd.read_csv("https://drive.google.com/file/d/1ioUp8979nNLh9h-CYVyJcRgAhxBhCLIm/view?usp=sharing")
+mapa['lat']=df[(df['Incident Year']==year_selected) & (df['Police District']==dist_selected)]['Latitude']
+mapa['lon']=df[(df['Incident Year']==year_selected) & (df['Police District']==dist_selected)]['Longitude']
+mapa=mapa.dropna()
+st.map(mapa)
+st.markdown("## The most insecure day.")
+val=df[(df['Incident Year']==year_selected) & (df['Police District']==dist_selected)]['Incident Day of Week']
+vals=pd.DataFrame(val)
+val_count = vals['Incident Day of Week'].value_counts()
+val_count=val_count.reindex(index=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])
 
-st.markdown('The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution.')
+fig = px.pie(df, values=val_count.values, names=val_count.index)
+st.plotly_chart(fig, use_container_width=True)
 
-mapa = pd.DataFrame()
-mapa['Date'] = df['Incident Date']
-mapa['Day'] = df['Incident Day of Week']
-mapa['Police District'] = df['Police District']
-mapa['Neighborhood'] = df['Analysis Neighborhood']
-mapa['Incident Category'] = df['Incident Category']
-mapa['Incident Subcategory'] = df['Incident Subcategory']
-mapa['Resolution'] = df['Resolution']
-mapa['lat'] = df['Latitude']
-mapa['lon'] = df['Longitude']
-mapa = mapa.dropna()
+cat=df[(df['Incident Year']==year_selected) & (df['Police District']==dist_selected)]['Incident Category']
+x2=cat.value_counts()
+y2=cat.unique()
 
-subset_data2 = mapa
-police_district_input = st.sidebar.multiselect(
-'Police District',
-mapa.groupby('Police District').count().reset_index()['Police District'].tolist())
-if len(police_district_input) > 0:
-    subset_data2 = mapa[mapa['Police District'].isin(police_district_input)]
+st.markdown("## Most frequent crimes.")
+fig2 = go.Figure(go.Bar(
+            x=x2,
+            y=y2,
+            orientation='h'))
 
-subset_data1 = subset_data2
-neighborhood_input = st.sidebar.multiselect(
-'Neighborhood',
-subset_data2.groupby('Neighborhood').count().reset_index()['Neighborhood'].tolist())
-if len(neighborhood_input) > 0:
-    subset_data1 = subset_data2[subset_data2['Neighborhood'].isin(neighborhood_input)]
+st.plotly_chart(fig2, use_container_width=True)
 
-subset_data = subset_data1
-incident_input = st.sidebar.multiselect(
-'Incident Category',
-subset_data1.groupby('Incident Category').count().reset_index()['Incident Category'].tolist())
-if len(incident_input) > 0:
-    subset_data = subset_data1[subset_data1['Incident Category'].isin(incident_input)]
-            
-subset_data    
+
+category=df['Incident Category']
+genre = st.radio(
+    "Have you suffered from a crime in these areas?",
+    ('No', 'Yes'))
+
+if genre == 'Yes':
+    st.write('Help us put the following information.')
+    year = st.slider('In what year did it happen?', 1930, 2022, 1930)
+    distrito=st.selectbox('In which area was it?', dist)
+    dia=st.selectbox('what day was it?', val_count.index)
+    crimen=st.selectbox('What happened?', category.unique())
     
-st.markdown('It is important to mention that any police district can answer to any incident, the neighborhood in which it happened is not related to the police district.')    
-st.markdown('Crime locations in San Francisco')
-st.map(subset_data)
-st.markdown('Crimes ocurred per day of the week')
-st.bar_chart(subset_data['Day'].value_counts())
-st.markdown('Crimes ocurred per date')
-st.line_chart(subset_data['Date'].value_counts())
-st.markdown('Type of crimes committed')
-st.bar_chart(subset_data['Incident Category'].value_counts())
-
-agree = st.button('Click to see Incident Subcategories')
-if agree:
-    st.markdown('Subtype of crimes committed')
-    st.bar_chart(subset_data['Incident Subcategory'].value_counts())
-
-
-st.markdown('Resolution status')
-fig1, ax1 = plt.subplots()
-labels = subset_data['Resolution'].unique()
-ax1.pie(subset_data['Resolution'].value_counts(), labels=labels, autopct='%1.1f%%', startangle=20)
-st.pyplot(fig1)
+    st.write("The crime was", crimen, "in", distrito,"it was a",dia,"in",year)
+    st.write("Thank You!")
+else:
+    st.write("Bye.")
 
 
